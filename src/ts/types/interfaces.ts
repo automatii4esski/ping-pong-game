7ae858405de01223e02b7cfb07393fa7c32b4564
Ivan <automatii4esski@gmail.com>
2023-04-11 20:53:37 +0500

    add types and interfaces

---
+++ src/ts/types/interfaces.ts
@@ -0,0 +1,15 @@
+export interface IPlayerConfig {
+  INIT_POS_X: number;
+  INIT_POS_Y: number;
+  WIDTH: number;
+  HEIGHT: number;
+  COLOR: string;
+  MOVE_STEP: number;
+}
+
+export interface IBallConfig {
+  INIT_POS_X: number;
+  INIT_POS_Y: number;
+  RADIUS: number;
+  COLOR: string;
+}
---
+++ src/ts/types/types.ts
@@ -0,0 +1,1 @@
+export type platformName = 'left' | 'right';
